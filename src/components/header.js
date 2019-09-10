ce5eedf8a9cdcd7c68ea831bd16002051e8dfb7f
Ørjan Blytt Polden <oerjanbpolden@gmail.com>
2019-09-10 14:45:41 +0200

    style: set darkmode color palette

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,8 @@ import styled from 'styled-components'
 const Header = styled.header`
     height: 64px;
     width: 100%;
-    background-color: #222f3e;
+    background-color: #2d383c;
+    box-shadow: 0px 0px 4px rgba(0,0,0,0.27);
 `
 
 export default Header
