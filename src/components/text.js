7b6ab6273baf8b9b52a0e8030369a0991506265d
Ørjan Blytt Polden <oerjanbpolden@gmail.com>
2019-09-12 10:52:48 +0200

    style: update fonts

--- src/components/text.js
+++ src/components/text.js
@@ -1,28 +1,27 @@
 import styled from 'styled-components'
 
-const fontHeading = '"Montserrat", sans-serif'
-const fontBody = '"Rubik", sans-serif'
+const fontHeading = '"Rubik", sans-serif'
+const fontBody = '"Karla", sans-serif'
 
 const fontColor = '#c8d6e5'
+const fontColorHeading = '#eef2f7'
 const linkColor = '#39ace7'
 
 const h1 = styled.h1`
     margin-top: 2rem;
     margin-bottom: 1rem;
-    color: ${fontColor};
+    color: ${fontColorHeading};
     font-family: ${fontHeading};
-    font-size: 2.5rem;
-    font-weight: 500;
+    font-size: 3rem;
     line-height: 3rem;
 `
 
 const h2 = styled.h2`
     margin-top: 1rem;
     margin-bottom: 1rem;
-    color: ${fontColor};
+    color: ${fontColorHeading};
     font-family: ${fontHeading};
-    font-size: 2rem;
-    font-weight: 500;
+    font-size: 1.5rem;
     line-height: 2.5rem;
 `
 
