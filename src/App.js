247fd55628e94947e59c3888883007b70d457c0e
Ørjan Blytt Polden <oerjanbpolden@gmail.com>
2019-09-12 12:40:28 +0200

    refactor: make pages follow lowercase-dash naming

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import Header from './components/header/header'
 
-import PageFlexbox from './pages/Flexbox'
-import PageCssGrid from './pages/CssGrid'
+import PageFlexbox from './pages/flexbox'
+import PageCssGrid from './pages/css-grid'
 
 function App() {
   return (
