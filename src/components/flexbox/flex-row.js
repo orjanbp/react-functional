27ad2981c2e827af57b53157abdce151f0957c83
Ørjan Blytt Polden <oerjanbpolden@gmail.com>
2019-09-13 13:30:50 +0200

    feat: add flex-row component

---
+++ src/components/flexbox/flex-row.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+const FlexRow = styled.div`
+    display: flex;
+    width: 100%;
+    flex-shrink: 0;
+    box-sizing: border-box;
+`
+
+export default FlexRow
