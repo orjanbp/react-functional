735eaf7c0d5ee3758699f9d8a2bcd769c185ade8
Ørjan Blytt Polden <oerjanbpolden@gmail.com>
2019-09-13 13:31:08 +0200

    refactor: make flexbox page actually show columns

--- src/pages/Flexbox.js
+++ src/pages/Flexbox.js
@@ -2,19 +2,35 @@ import React from 'react'
 
 import Page from '../components/page'
 import Text from '../components/text'
-import FlexColumn from '../components/flexbox/flex-column'
 import Outline from '../components/outline'
+
+import FlexRow from '../components/flexbox/flex-row'
+import FlexColumn from '../components/flexbox/flex-column'
+
+// Dummy element to foreach a row of cols on page
+let cols = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
 const PageFlexbox = () => (
   <Page>
-    <FlexColumn col="12" area="a" style={{ height: 300 }}>
+    <FlexRow>
+      <FlexColumn col="12" style={{ height: 200 }}>
+        <Outline>
+          <Text.p>12 Col</Text.p>
+        </Outline>
+      </FlexColumn>
+    </FlexRow>
+    <FlexRow>
+      {cols.map(col => (
+        <FlexColumn col="1">
+          <Outline>
+            <Text.p>1 Col</Text.p>
+          </Outline>
+        </FlexColumn>
+      ))}
+    </FlexRow>
+    <FlexColumn col="9" style={{ height: 400 }}>
       <Outline>
-        <Text.p>Header</Text.p>
-      </Outline>
-    </FlexColumn>
-    <FlexColumn col="9" style={{}}>
-      <Outline>
-        <Text.p>Article</Text.p>
+        <Text.p>9 Col</Text.p>
         <Text.h1>Heading</Text.h1>
         <Text.h2>Subheading</Text.h2>
         <Text.p>Paragraph</Text.p>
@@ -23,14 +39,19 @@ const PageFlexbox = () => (
         </Text.p>
       </Outline>
     </FlexColumn>
-    <FlexColumn col="3" style={{ minHeight: 600 }}>
+    <FlexColumn col="3">
       <Outline>
-        <Text.p>Sidebar</Text.p>
+        <Text.p>3 Col</Text.p>
       </Outline>
     </FlexColumn>
-    <FlexColumn col="12" style={{ height: 200 }}>
+    <FlexColumn col="6" style={{ height: 200 }}>
       <Outline>
-        <Text.p>Footer</Text.p>
+        <Text.p>6 Col</Text.p>
+      </Outline>
+    </FlexColumn>
+    <FlexColumn col="6" style={{ height: 200 }}>
+      <Outline>
+        <Text.p>6 Col</Text.p>
       </Outline>
     </FlexColumn>
   </Page>
