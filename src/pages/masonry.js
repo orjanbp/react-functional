82acd312c0ec322bb5677a2ded0df6c9340c8b66
Ørjan Blytt Polden <oerjanbpolden@gmail.com>
2019-09-13 13:20:28 +0200

    chore: remove deparacated function

--- src/pages/masonry.js
+++ src/pages/masonry.js
@@ -4,10 +4,6 @@ import Page from '../components/page'
 import MasonWall from '../components/masonry/mason-wall'
 import DogImageMason from '../components/masonry/dog-image-mason'
 
-function getRandomBoxHeight() {
-  return Math.floor(Math.random() * (300 - 200) + 200)
-}
-
 const PageMasonry = () => (
   <Page>
     <MasonWall>
